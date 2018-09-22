6719685fbc7f895a909e2515753c02794591595e
SoojungChae <naan_ace@naver.com>
2018-09-22 23:43:57 +0900

    Block unnecessary things

--- project/mvcPattern/src/main/java/com/soocompany/mvc/HomeController.java
+++ project/mvcPattern/src/main/java/com/soocompany/mvc/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.soocompany.dto.BoardVO;
+import com.soocompany.application.board.model.BoardVO;
 import com.soocompany.service.BoardServiceImpl;
 
 /**
@@ -24,83 +24,83 @@ import com.soocompany.service.BoardServiceImpl;
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-	// 서비스에서 데이터 가져오기
-	@Autowired
-	private BoardServiceImpl service;
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		List<BoardVO> boardList = service.selectBoard();
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate );
-
-		if (boardList.size() != 0)
-			model.addAttribute("title", boardList.get(0).getTitle());
-
-		return "home";
-	}
-
-	@RequestMapping(value="/board")
-	public ModelAndView showBoard(Map<String, String> param) {
-		ModelMap model = new ModelMap();
-
-		List<BoardVO> list = service.selectBoardList();
-
-		model.addAttribute("list", list);
-
-		return new ModelAndView("board/boardMain", model);
-	}
-
-	@RequestMapping(value="/contents")
-	public ModelAndView write(Map<String, String> param) {
-		ModelAndView mav = new ModelAndView("contents");
-		mav.addObject("method", "POST");
-		mav.addObject("actionUrl", "/contents");
-		return mav;
-	}
-
-	@RequestMapping(value="/contents", method = RequestMethod.POST)
-	public String writeBoard(@ModelAttribute BoardVO boardVO) {
-		service.writeBoard(boardVO);
-		return "redirect:/board";
-	}
-
-	@RequestMapping(value="/contents/{id}")
-	public ModelAndView read(@PathVariable int id) {
-		ModelAndView mav = new ModelAndView("contents");
-		mav.addObject("board", service.boardDetail(id));
-		mav.addObject("isRead", true);
-		return mav;
-	}
-
-	@RequestMapping(value="/edit/{id}")
-	public ModelAndView edit(@PathVariable Integer id) {
-		ModelAndView mav = new ModelAndView("contents");
-		mav.addObject("method", "PUT");
-		mav.addObject("actionUrl", "/edit/" + id);
-		mav.addObject("board", service.boardDetail(id));
-		return mav;
-	}
-
-	@RequestMapping(value="/edit/{id}", method = RequestMethod.POST)
-	public String edit(@PathVariable Integer id, @ModelAttribute BoardVO boardVO) {
-		//ModelAndView mav = new ModelAndView("contents");
-		service.editBoard(id, boardVO);
-		return "redirect:/edit/" + id;
-	}
-
-	@RequestMapping(value="/delete/{id}")
-	public String delete(@PathVariable int id) {
-		service.deleteBoard(id);
-		return "redirect:/board/";
-	}
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//
+//	// 서비스에서 데이터 가져오기
+//	@Autowired
+//	private BoardServiceImpl service;
+//
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//
+//		List<BoardVO> boardList = service.selectBoard();
+//		String formattedDate = dateFormat.format(date);
+//
+//		model.addAttribute("serverTime", formattedDate );
+//
+//		if (boardList.size() != 0)
+//			model.addAttribute("title", boardList.get(0).getTitle());
+//
+//		return "home";
+//	}
+//
+//	@RequestMapping(value="/board")
+//	public ModelAndView showBoard(Map<String, String> param) {
+//		ModelMap model = new ModelMap();
+//
+//		List<BoardVO> list = service.selectBoardList();
+//
+//		model.addAttribute("list", list);
+//
+//		return new ModelAndView("board/boardMain", model);
+//	}
+//
+//	@RequestMapping(value="/contents")
+//	public ModelAndView write(Map<String, String> param) {
+//		ModelAndView mav = new ModelAndView("contents");
+//		mav.addObject("method", "POST");
+//		mav.addObject("actionUrl", "/contents");
+//		return mav;
+//	}
+//
+//	@RequestMapping(value="/contents", method = RequestMethod.POST)
+//	public String writeBoard(@ModelAttribute BoardVO boardVO) {
+//		service.writeBoard(boardVO);
+//		return "redirect:/board";
+//	}
+//
+//	@RequestMapping(value="/contents/{id}")
+//	public ModelAndView read(@PathVariable int id) {
+//		ModelAndView mav = new ModelAndView("contents");
+//		mav.addObject("board", service.boardDetail(id));
+//		mav.addObject("isRead", true);
+//		return mav;
+//	}
+//
+//	@RequestMapping(value="/edit/{id}")
+//	public ModelAndView edit(@PathVariable Integer id) {
+//		ModelAndView mav = new ModelAndView("contents");
+//		mav.addObject("method", "PUT");
+//		mav.addObject("actionUrl", "/edit/" + id);
+//		mav.addObject("board", service.boardDetail(id));
+//		return mav;
+//	}
+//
+//	@RequestMapping(value="/edit/{id}", method = RequestMethod.POST)
+//	public String edit(@PathVariable Integer id, @ModelAttribute BoardVO boardVO) {
+//		//ModelAndView mav = new ModelAndView("contents");
+//		service.editBoard(id, boardVO);
+//		return "redirect:/edit/" + id;
+//	}
+//
+//	@RequestMapping(value="/delete/{id}")
+//	public String delete(@PathVariable int id) {
+//		service.deleteBoard(id);
+//		return "redirect:/board/";
+//	}
 	
 }
